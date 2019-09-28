dee1d95db5fef64c872274568343434d86416331
olya <olya_r@tut.by>
2019-09-28 20:59:00 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,22 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.minValue = min;
+        this.maxValue = max;
     }
 
     guess() {
-
+        return (this.candidate = Math.round((this.minValue + this.maxValue) / 2));
     }
 
     lower() {
-
+        //this.maxValue = this.candidate - 1;
+        this.maxValue = this.candidate;
     }
 
     greater() {
-
+        //this.minValue = this.candidate + 1;
+        this.minValue = this.candidate;
     }
 }
 
